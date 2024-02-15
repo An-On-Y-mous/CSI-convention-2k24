b84a7563e61bde8668430b106ee231f10b885016
shadowctrl <shadowctrl@outlook.com>
2024-02-15 06:40:33 +0530

    Register Complete

--- src/components/register-form/reg-complete.jsx
+++ src/components/register-form/reg-complete.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "../register-form/reg-complete.css";
+import Lottie from "lottie-react";
+import tick from "../../assets/verified.json";
 
 const RegComplete = () => {
   return (
-    <div className="complete">
-      <h1>Registration Successful!<br/><span>Thank You for registering</span></h1>
+    <div className="verify-title">
+      <h1>
+        Registration Successful
+        <Lottie
+          animationData={tick}
+          style={{
+            height: "100px",
+            width: "100px",
+          }}
+          loop={1}
+        />
+      </h1>
+      <h3>Thank you for registering</h3>
     </div>
   );
 };
